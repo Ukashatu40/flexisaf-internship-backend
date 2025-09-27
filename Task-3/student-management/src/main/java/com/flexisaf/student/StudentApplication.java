67430030987e7ce6318aabcd64daf232c731e8c3
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-09-27 10:48:07 +0100

    updated backend

--- Task-3/student-management/src/main/java/com/flexisaf/student/StudentApplication.java
+++ Task-3/student-management/src/main/java/com/flexisaf/student/StudentApplication.java
@@ -19,7 +19,7 @@ public class StudentApplication {
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
 	            registry.addMapping("/**")
-	                    .allowedOrigins("http://localhost")
+	                    .allowedOrigins("http://localhost:5173")
 	                    .allowedMethods("GET", "POST", "PUT", "DELETE")
 	                    .allowedHeaders("*");
 	        }
