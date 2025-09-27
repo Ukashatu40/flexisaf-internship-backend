7c5203d0adf08d4f8a106e5dc338564cee56fc1b
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-09-27 13:02:42 +0100

    Update CORS for Vercel

--- Task-3/student-management/src/main/java/com/flexisaf/student/StudentApplication.java
+++ Task-3/student-management/src/main/java/com/flexisaf/student/StudentApplication.java
@@ -19,7 +19,7 @@ public class StudentApplication {
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
 	            registry.addMapping("/**")
-	                    .allowedOrigins("http://localhost:5173")
+	                    .allowedOrigins("http://localhost:5173", "https://flexisaf-internship-frontend.vercel.app/")
 	                    .allowedMethods("GET", "POST", "PUT", "DELETE")
 	                    .allowedHeaders("*");
 	        }
