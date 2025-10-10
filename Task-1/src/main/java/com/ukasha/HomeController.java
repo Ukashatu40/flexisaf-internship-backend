6a4e98d80e3552a6685f6e34b6f06abf55eeef32
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-10-10 15:28:27 +0100

    Done with this one

--- Task-1/src/main/java/com/ukasha/HomeController.java
+++ Task-1/src/main/java/com/ukasha/HomeController.java
@@ -1,7 +1,7 @@
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
     @RequestMapping("/")
     public String index() {
         return "index.html";
