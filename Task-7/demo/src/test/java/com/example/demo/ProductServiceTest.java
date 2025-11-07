b44d6ae7985202201d6012bb9ad1e371d48b8074
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-11-07 19:39:14 +0100

    feat: Test with Mockito and jUnit5

--- Task-7/demo/src/test/java/com/example/demo/ProductServiceTest.java
+++ Task-7/demo/src/test/java/com/example/demo/ProductServiceTest.java
@@ -1,4 +1,43 @@
 package com.example.demo;
 
+import com.example.demo.dto.ProductRequest;
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    ProductService productService;
+
+    @Test
+    void testProductRepository(){
+        Product product = new Product("T-shirt", 2000.00, 3);
+
+        when(productRepository.save(product)).thenReturn(product);
+
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setName(product.getName());
+        productRequest.setPrice(product.getPrice());
+        productRequest.setStockQuantity(product.getStockQuantity());
+
+        Product result = productService.createProduct(productRequest);
+
+        assertEquals("T-shirt", result.getName());
+        assertEquals(2000.00, result.getPrice());
+        assertEquals(3, result.getStockQuantity());
+
+    }
+
 }
